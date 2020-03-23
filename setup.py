2166a9f2752e25f5f48b2c55d3b61b92e64af5c6
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-03-23 08:15:26 -0400

    py3 changes

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyxhook',
-    version='1.0.0',
+    version='2.0.0',
     description='Packaged version of pyxhook',
-    maintainer='patamechanix',
+    maintainer='rpigu-i',
     license='GNU version 2',
-    url='https://github.com/patamechanix/pyxhook/',
+    url='https://github.com/rpigu-i/pyxhook/',
     package_dir={'': 'src'},
     include_package_data=True,
     packages=find_packages('src'),
